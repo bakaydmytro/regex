8460a1f0617194397f17f3b6f830fc01c7584db6
Dmytro Bakai <dbaka@softserveinc.com>
2023-09-08 16:04:48 +0300

    UC#10 Implement regex

---
+++ src/main/java/org/example/ValidationService.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.regex.Pattern;
+
+public class ValidationService {
+
+    public boolean valid(String inputValue, int maxLength) {
+        String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!\"#$%'()*+,-./:;<=>?@\\[\\]^_`{|}~])(?!.*\\s).{1,"+maxLength+"}$";
+        return Pattern.matches(regex, inputValue);
+    }
+
+}
